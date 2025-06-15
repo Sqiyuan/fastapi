aaafc4d3f38b5e866868da332cef55af06f129b1
Sqiyuan <qiy14999@163.com>
2025-06-15 22:48:46 +0800

    task group 学习

--- src/02concurrent.py
+++ src/02concurrent.py
@@ -25,6 +25,32 @@ async def main_2():
     for task in tasks:
         print(await task)
 
+async def greet1(name, delay):
+    await asyncio.sleep(delay)
+    if name == "Alice":
+        raise ValueError("Alice is not a valid name")
+    return f"Hello, {name}!"
+
+@async_timed
+#用taskgroup创建任务组
+async def main_3():
+    try:
+        async with asyncio.TaskGroup() as tg:
+            task1 = tg.create_task(greet1("Alice", 2))
+            task2 = tg.create_task(greet1("Bob", 3))
+            task3 = tg.create_task(greet1("Charlie", 4))
+    except Exception as e:
+        print(e)
+    # 协程正常运行结束
+    # print(task1.result())
+    # print(task2.result())
+    # print(task3.result())
+    # 出现异常导致提前结束协程（sleep时间短于异常也会done）
+    # done: 返回协程是否结束
+    # cancelled: 返回协程是否被取消
+    print(task1.done())
+    print(task2.cancelled())
+    print(task3.cancelled())
 
 if  __name__ == "__main__":
-    asyncio.run(main_2())
+    asyncio.run(main_3())
